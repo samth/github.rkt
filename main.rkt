8cf742ff777986d43914b559ba074ecef0c95729
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-06-10 17:44:31 -0400

    Lots of progress

--- main.rkt
+++ main.rkt
@@ -1,23 +1,11 @@
 #lang racket/base
 
-(require racket/class json net/url racket/port net/base64 racket/match racket/trait)
+(require racket/class json net/url racket/port net/base64 racket/match racket/trait
+         racket/format)
 
 (provide client%)
 
 (define ua-header "User-Agent: Racket github package; github.com/samth/github.rkt")
-
-(define (do-post token title body url)
-  (define auth-header (format "Authorization: bearer ~a" token))
-  (define json (jsexpr->bytes (hash 'title title 'body body)))
-  (define result-port
-    (post-impure-port
-     (string->url url)
-     json
-     (list auth-header ua-header)))
-  (define headers (purify-port result-port))
-  (when #f
-    (printf "Headers: \n~a" headers))
-  (bytes->jsexpr (port->bytes result-port)))
 
 (define (http-authorization-header username password)
   (let ([username-bytes (string->bytes/utf-8 username)]
@@ -28,45 +16,62 @@
       (base64-encode
        (bytes-append username-bytes #":" password-bytes) #"")))))
 
-(define http-trait 
-  (trait
+(define (http-mixin %)
+  (class %
    (inherit-field endpoint)
-   (inherit mk-auth)
+   (inherit mk-auth has-auth?)
+    (super-new)
    (define/public (request method url [data 'null] #:auth [auth #f])
-     (define auth-header (and auth (mk-auth (eq? auth 'basic))))
+     (define auth-header 
+       (case auth
+         [(#f) #f]
+         [(basic) (mk-auth #t)]
+         [(maybe) (and (has-auth?) (mk-auth))]
+         [(#t) (mk-auth)]))
      (define hs (filter values (list auth-header ua-header)))
+     (define (simple f)
+       (define result-port (f (string->url url) hs))
+       (purify-port result-port) ;; currently ignored
+       (bytes->jsexpr (port->bytes result-port)))
+     (define (simple/post f #:data [data #""])
+       (simple (λ (u hs) (f u data hs))))
      (match method
        ['get (define-values (p headers)
                (get-pure-port/headers 
                 (string->url url) hs
                 #:redirections 5))
              (bytes->jsexpr (port->bytes p))]
-       ['post (define result-port (post-impure-port (string->url url)
-                                                    (jsexpr->bytes data)
-                                                    hs))
-              (purify-port result-port) ;; currently ignored
-              (bytes->jsexpr (port->bytes result-port))]
+       ['post (simple/post #:data (jsexpr->bytes data))]
        ['patch (request 'post url data #:auth auth)] ;; net/url doesn't support PATCH
-       ['head (define result-port (head-impure-port (string->url url) hs))
-              (purify-port result-port) ;; currently ignored
-              (bytes->jsexpr (port->bytes result-port))]))
+       ['head (simple head-pure-port)]
+       ['delete (simple delete-pure-port)]
+       ['put (simple/post put-pure-port)]))
    
    (define/public (post url data #:auth [auth #f])
-     (request 'post (string-append endpoint url) (jsexpr->bytes data)
+     (request 'post (string-append endpoint url) data
               #:auth auth))
-   
-   (define/public (get url #:auth [auth #f])
-     (request 'get (string-append endpoint url)
-              #:auth auth))))
+    (define/public (patch url data #:auth [auth #f])
+     (request 'patch (string-append endpoint url) data
+              #:auth auth))
+    (define-syntax-rule (def-http method ...)
+      (begin (define/public (method url #:auth [auth #f])
+               (request 'method (string-append endpoint url) #:auth auth))
+             ...))
+   (def-http get put delete head)))
 
 (define no-auth-trait
   (trait
-   (define/public (mk-auth) #f)))
+   (define/public (mk-auth [x #f]) #f)
+   (define/public (has-auth?) #f)))
 
 (define auth-trait
   (trait
-   (define-values (password login oauth-token)
-     (values (get-field password this) (get-field login this) (get-field oauth-token this)))
+   (inherit-field password login oauth-token)
+   (define/public (has-auth? [basic? #f])
+     (cond [(and basic? password login) #t]
+           [basic? #f]
+           [(or oauth-token (and password login)) #t]
+           [else #f]))
    (define/public (mk-auth [basic? #f])
       (cond [(and basic? password login)
              (http-authorization-header login password)]
@@ -79,29 +84,96 @@
                  (format "Authorization: bearer ~a" oauth-token)
                  (http-authorization-header login password))]))))
 
-(define client%
-  (class ((trait->mixin (trait-sum auth-trait http-trait)) object%)
-    (inherit get post)
-    (init-field [login #f] [oauth-token #f] [password #f]
-                [endpoint "https://api.github.com"])
-    (super-new)
-    
-    (define/public (meta) (get "/meta"))
-    
-    (define/public (get-token [options (hash)])
-      (post "/authorizations" options #:basic #t))
+;; for general github methods that require no authorization
+(define gh-trait
+  (trait
+   (inherit get post)
+   (define/public (meta) (get "/meta"))
+   (define/public (rate-limit) (get "/rate_limit"))))
+
+(define (->symbol v) (if (symbol? v) v (string->symbol (~a v))))
+
+(define gist-trait
+  (trait
+   (inherit get post put delete)
+   ;; #:user (or/c #f Username)
+   (define/public (gists #:user [u #f])
+     (match u
+       [(? string?) (get (format "/users/~a/gists" u) #:auth 'maybe)]
+       [#f          (get "/gists/public")]))
+   (define/public (create-gist #:public [public? #t]
+                               #:desc [desc 'null]
+                               #:auth [auth 'maybe]
+                               files)
+     (post "/gists" #:auth auth
+           (hash 'public public?
+                 'description desc
+                 'files (for/hash ([(name value) files])
+                          (values (->symbol 
+                                   (or name (object-name value)
+                                       (error 'create-gist "file requires a name")))
+                                  (hash 'content
+                                        (match value
+                                          [(? string?) value]
+                                          [(? bytes?) (bytes->string/utf-8 value)]
+                                          [(? input-port?) (port->string value)]
+                                          [_ (~a value)])))))))
+   ;; use #f as the contents to remove a file
+   (define/public (edit-gist #:desc [desc 'null] files)
+     (post "/gists" #:auth #t
+           (hash 'description desc
+                 'files (for/hash ([(name value) files])
+                          (values (->symbol 
+                                   (or name (object-name value)
+                                       (error 'create-gist "file requires a name")))
+                                  (hash 'content
+                                        (match value
+                                          [(? string?) value]
+                                          [(? bytes?) (bytes->string/utf-8 value)]
+                                          [(? input-port?) (port->string value)]
+                                          [#f 'null]
+                                          [_ (~a value)])))))))
+   (define/public (fork-gist id)
+     (post (format "/gists/~a/forks" id) #:auth #t))
+   (define/public (star-gist id)
+     (put (format "/gists/~a/star" id) #:auth #t))
+   (define/public (gist-star? id)
+     (get (format "/gists/~a/star" id) #:auth #t))
+   (define/public (delete-gist id)
+     (delete (format "/gists/~a" id) #:auth #t))))
+
+;; for performing actions on behalf of a user
+(define client-trait 
+  (trait 
+   (inherit get post)
+   
+   (define/public (get-token [options (hash)])
+      (send this post "/authorizations" options #:basic #t))
     
     (define/public (authorizations [n #f])
       (if n
-          (get (format "/authorizations/~a" n) #:basic #t)
-          (get "/authorizations" #:basic #t)))
-    
-    
-    
-    
-    
+          (send this get (format "/authorizations/~a" n) #:auth 'basic)
+          (send this get "/authorizations" #:auth 'basic)))
+        
     (define/public (post-bug repo title [body 'null] [options (hash)])
-      (post (format "/repos/~a/issues" repo)
+      (send this post (format "/repos/~a/issues" repo)
             (hash-set* options 'title title 'body body)))))
+ 
+(define github% 
+  (class object% 
+    (init-field [endpoint "https://api.github.com"])
+    (super-new)))
 
-(define me (new client% [login "samth"] [oauth-token "token"]))
+(define client-state%
+  (class github%    
+    (init-field [login #f] [oauth-token #f] [password #f])
+    (super-new)))
+
+(define methods (trait->mixin (trait-sum gh-trait gist-trait)))
+(define client-methods (trait->mixin (trait-sum gh-trait gist-trait client-trait)))
+
+(define client% (client-methods (http-mixin ((trait->mixin auth-trait) client-state%))))
+(define octokit%  (methods (http-mixin ((trait->mixin no-auth-trait) github%))))
+
+(provide client% octokit%)
+;(define me (new client% [login "samth"] #;[oauth-token "token"] [password ""]))
